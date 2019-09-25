800bc7cbbc108db17a4b28988e1292f164dcc4c8
lIotaMiu <42389128+liotamiu@users.noreply.github.com>
2019-09-25 10:54:06 +0200

    Ajout de javadoc

--- src/TableauEntier.java
+++ src/TableauEntier.java
@@ -1,11 +1,27 @@
+/**
+ * Class permetent de geres les tableau a 2 dimenstion
+ */
 public class TableauEntier {
 
+    /**
+     * Tableau d'entier a 2 dimenstion
+     */
     private int[][] t;
 
+    /**
+     * contruteur par default
+     * @param t tableau d'entier a deux dimenstion
+     */
     public TableauEntier(int[][] t) {
         this.t = t;
     }
 
+    /**
+     * fonction pour optenir un valeur dans le tableau
+     * @param l position sur le premier tableau
+     * @param c position sur le deusieme tableau
+     * @return la valeur au position donné, si sortie de tableau alors retour la valeur minimal d'un entier
+     */
     int valeurA(int l, int c) {
         try {
             return this.t[l][c];
@@ -14,6 +30,10 @@ public class TableauEntier {
         }
     }
 
+    /**
+     * fonction pour openir la taille du deuxeme tableau
+     * @return la taille en entier
+     */
     int getLargeur() {
         try {
             return this.t[0].length;
@@ -22,6 +42,10 @@ public class TableauEntier {
         }
     }
 
+    /**
+     * fonction pour optenir la taille du premier tableau
+     * @return la taille en entier
+     */
     int getHauteur() {
         return this.t.length;
     }
