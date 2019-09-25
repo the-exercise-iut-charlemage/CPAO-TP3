c483055df4675acb6a8d783b220f1786ae16b8ee
lIotaMiu <42389128+liotamiu@users.noreply.github.com>
2019-09-25 10:40:48 +0200

    update tableau entier

--- src/TableauEntier.java
+++ src/TableauEntier.java
@@ -1,2 +1,23 @@
 public class TableauEntier {
+
+    private int[][] t;
+
+    public TableauEntier(int[][] t) {
+        // todo
+    }
+
+    int valeurA(int l, int c) {
+        // todo
+        return -1;
+    }
+
+    int getLargeur() {
+        // todo
+        return -1;
+    }
+
+    int getHauteur() {
+        // todo
+        return -1;
+    }
 }
